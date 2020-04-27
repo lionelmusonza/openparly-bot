13efc4f1cafbbb07b2602f0e95b4cc2970f730c0
lio <lionel@localhost.localdomain>
2020-04-28 00:49:08 +0300

    zvitsva

--- receiveMessage.php
+++ receiveMessage.php
@@ -7,23 +7,39 @@ $response =  new MessagingResponse();
 $guess = $_REQUEST['Body'];
 $pick = rand(1,5);
 
-if (in_array($guess, ['gender','breakdown'])) {
-   $response->message('kubva ku api');
-} elseif (in_array($guess, ['gender','breakdown'])) {
-   $response->message("kubva ku api");
+if (in_array($guess, ['gender breakdown'])) {
+   $response->message('sexUpdate');
+} elseif (in_array($guess, ['provinces'])) {
+   $response->message("CasesProvince");
+
+} elseif (in_array($guess, ['update summary'])) {
+   $response->message("UpdateSummary");
+
+} elseif (in_array($guess, ['positive cases'])) {
+   $response->message("apicase");
+
+
+} elseif (in_array($guess, ['daily stats'])) {
+   $response->message("apiday");
+
+
+} elseif (in_array($guess, ['dateUpdate'])) {
+   $response->message("dateUpdate");
+
+
+
+
+
+
 } else {
    $response->message("Correct information is critical. This service provides the official and up to date information on the status of COVID-19 in Zimbabwe from the Open Parly
    Reply with one of the words in the list below to get the correct information on the topic:  
-   News 🆕
-   Cases 🗓
-   Myths 🛑
-   About ℹ️
-   Prevention 👍
-   Symptoms 🤒
-   Treatment 🏥
-   Testing 🔬 
-   Conditions 📁
-   Check 💟 - COVID-19 Risk assessment
+   gender breakdown
+   provinces
+   update summary
+   positive cases
+   daily stats
+   dateUpdate
    Share this service ➡️");
 }
 
