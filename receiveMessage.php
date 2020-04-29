7fe86ae355233345c0785a7ef99f8a413410b8e1
lio <lionel@localhost.localdomain>
2020-04-29 19:28:21 +0300

    zvitsva

--- receiveMessage.php
+++ receiveMessage.php
@@ -3,6 +3,13 @@ require_once './vendor/autoload.php';
 
 use Twilio\TwiML\MessagingResponse;
 
+
+$serverName = "db4free.net:3306";
+$username = "nyaradzo";
+$password = "defaultnyaradzopassword";
+$dbname = "nyaradzo";
+$conn = new mysqli($serverName, $username, $password, $dbname);
+
 $response =  new MessagingResponse();
 $msg = $_REQUEST['Body'];
 
@@ -15,7 +22,7 @@ switch ($msg) {
        $response->message("1. Spraying chlorine or alcohol on the skin kills viruses in the body\nApplying alcohol or chlorine to the body can cause harm, especially if it enters the eyes or mouth. Although people can use these chemicals to disinfect surfaces, they should not use them on the skin.\nThese products cannot kill viruses within the body.\n\n8. Only older adults and young people are at risk\nSARS-CoV-2, like other coronaviruses, can infect people of any age. However, older adults and individuals with preexisting health conditions, such as diabetes or asthma, are more likely to become severely ill.\n\n3. Children cannot get COVID-19\nAll age groups can contract SARS-CoV-2.\nSo far, most cases have been in adults, but children are not immune. In fact, preliminary evidence suggests that children are just as likely to contract it, but their symptoms tend to be less severe.\n\n4. COVID-19 is just like the flu\nSARS-CoV-2 causes an illness that does have flu-like symptoms, such as aches, a fever, and a cough. Similarly, both COVID-19 and the flu can be mild, severe, or, in rare cases, fatal. Both can also lead to pneumonia.\nHowever, the overall profile of COVID-19 is more serious. Estimates vary, but its mortality rate seems to be between about 1% and 3%.\nAlthough scientists are still working out the exact mortality rate, it is likely to be many times higher than that of seasonal flu.\n\n5. Everyone with COVID-19 dies\nThis statement is untrue. ");
        break;
    case 3:
-       $response->message(requestDataAPI($uri));
+       $response->message(allocateCounsellor(trim($_REQUEST['From'],"whatsapp:+"),$conn));
        break;
    case 4:
        $response->message(requestDataAPI($uri));
@@ -44,21 +51,7 @@ switch ($msg) {
   
    default:
    $response->message("Correct information is critical. This service provides the official and up to date information on the status of COVID-19 in Zimbabwe from the OpenParly.
-   \nReply with one of the numbers corresponding to a topic in the list below to get the correct information on the topic: 
-
-   1. Symptoms.
-   2. Myths. 
-   3. Need  Conselling? 
-   4. Precautions/Preventative Measures.
-   5. Gender Breakdown of Cases.
-   6. Cases by Provinces.
-   7. Update Summary.
-   8. Positive Cases.
-   9. Daily Stats.
-   10.Contact Rapid Response Team. 
-   11.Mode of Transmission.
-   
-   Share this service ➡️");
+   \nReply with one of the numbers corresponding to a topic in the list below to get the correct information on the topic:\n\n 1. Symptoms.\n 2. Myths.\n 3. Need  Conselling?\n 4. Precautions/Preventative Measures.\n 5. Gender Breakdown of Cases.\n 6. Cases by Provinces.\n 7. Update Summary.\n 8. Positive Cases.\n 9. Daily Stats.\n 10.Contact Rapid Response Team.\n 11.Mode of Transmission.\nShare this service ➡️");
 
    
 
@@ -84,11 +77,34 @@ function DecodeDataAPI($id,$jsonobj){}
 
 
 //shortList for counselling
-function allocateCounsellor($counselorRequest){
-   $number = array("1","2","3","4","5","6","7","8"); 
-   $length = count($number);
-   $number[$length] = $number[0];
-   $availableCounselor = $number[0];
-   array_shift($number);
-   return $availableCounselor;
+function allocateCounsellor($wanum,$conn){
+   $waNumber = $wanum;
+   $chatID = rand(8000,324094230);
+   // Check connection
+   if ($conn->connect_error) {
+      die("Connection failed: " . $conn->connect_error);
+   }
+
+
+   $sql = "INSERT INTO todos(phone,chatID)
+   VALUES ($waNumber, $chatID)";
+   $sql2 = "SELECT * FROM todos WHERE phone='$waNumber'";
+
+   if($conn->query($sql2)->num_rows===0){
+      if ($conn->query($sql) === TRUE) {
+         //echo "New record created successfully";
+         $resp = 'A counsellor will contact you as soon as possible use this verification number when asked: ' . $chatID;
+      } else {
+         echo "Error: " . $sql . "<br>" . $conn->error;
+      }
+   }
+   else
+      $resp = 'A previous request is still pending, a counsellor will attend to you as soon as available. ';
+      
+
+   
+
+   //$conn->close();
+   
+   return $resp;
 }
